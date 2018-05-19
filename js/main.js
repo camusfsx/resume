29e272d7f060413f5252ead8bdf5169bef027d53
camus <f1992863292@outlook.com>
2018-05-19 18:29:37 +0800

    gamma

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-
+var itemType = document.getElementsByClassName('itemType')[0].children
+var itemBar = document.getElementsByClassName('itemBar')[0]
+itemType[0].onclick=function(){
+    itemBar.style.width = '90px'
+    itemBar.style.marginLeft = '-5px'
+}
+itemType[1].onclick=function(){
+    itemBar.style.width = '105px'
+    itemBar.style.marginLeft = '90px'
+}
+itemType[2].onclick=function(){
+    itemBar.style.width = '105px'
+    itemBar.style.marginLeft = '200px'
+}
