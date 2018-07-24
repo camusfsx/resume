ed11184d1ec4421e61dfd9393ea03f7c4a5e54b5
camus <f1992863292@outlook.com>
2018-07-24 11:19:00 +0800

    bugfix

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
-var itemType = document.getElementsByClassName('itemType')[0].children
-var itemBar = document.getElementsByClassName('itemBar')[0]
-itemType[0].onclick=function(){
-    itemBar.style.width = '90px'
-    itemBar.style.marginLeft = '-5px'
+let itemType = document.getElementsByClassName('itemType')[0].children
+,itemBar = document.getElementsByClassName('itemBar')[0]
+itemType[0].onclick=()=>{
+    itemBar.setAttribute(
+        `style`,
+        `width:90px;
+         margin-left:0px;`
+    )
 }
-itemType[1].onclick=function(){
-    itemBar.style.width = '105px'
-    itemBar.style.marginLeft = '90px'
+itemType[1].onclick=()=>{
+    itemBar.setAttribute(
+        `style`,
+        `width:105px;
+         margin-left:90px;`
+    )
 }
-itemType[2].onclick=function(){
-    itemBar.style.width = '105px'
-    itemBar.style.marginLeft = '200px'
+itemType[2].onclick=()=>{
+    itemBar.setAttribute(
+        `style`,
+        `width:105px;
+         margin-left:200px;`
+    )
 }
